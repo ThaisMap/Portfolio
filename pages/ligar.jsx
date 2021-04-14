746f087884dc33f5342458dbe7b10ad0214f8d96
Thais <thais.map@hotmail.com>
2021-04-14 10:56:07 -0300

    fix import

--- pages/ligar.jsx
+++ pages/ligar.jsx
@@ -1,4 +1,4 @@
-import styles from '../styles/ligar.module.css';
+import styles from "../styles/projects/ligar.module.css";
 
 export default function Ligar() {
   return <div className={styles.container}>Ligar</div>;
